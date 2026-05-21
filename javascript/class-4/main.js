9c61509059c1ce3a3b2c46fa9fe4bb0b0b4c03f8
Tazeemabbas <tazanu2003@gmail.com>
2026-05-21 08:49:02 +0530

    Logical or operations

--- javascript/class-4/main.js
+++ javascript/class-4/main.js
@@ -2,3 +2,8 @@ console.log(true && true); // true
 console.log(true && false); // false
 console.log(false && true); // false
 console.log(false && false); // false   
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false   
