2be1a912dbd513a880460545dc1f16de25bae80f
Tazeemabbas <tazanu2003@gmail.com>
2026-05-25 09:07:17 +0530

    Days using numbers inn switch case

--- javascript/Class-6/main.js
+++ javascript/Class-6/main.js
@@ -1,25 +1,25 @@
-let week = "Friday";
+let week = 2
 
 switch (week) {
-    case "Monday":
+    case 0:
         console.log("Today is Monday");
         break;
-    case "Tuesday":
+    case 1:
         console.log("Today is Tuesday");
         break;
-    case "Wednesday":
+    case 2:
         console.log("Today is Wednesday");
         break;
-    case "Thursday":
+    case 3:
         console.log("Today is Thursday");
         break;
-    case "Friday":
+    case 4:
         console.log("Today is Friday");
         break;
-    case "Saturday":
+    case 5:
         console.log("Today is Saturday");
         break;
-    case "Sunday":
+    case 6:
         console.log("Today is Sunday");
         break;  
     default:
