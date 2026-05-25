70406e1443b8c61c5236f476751f277f7035de00
Tazeemabbas <tazanu2003@gmail.com>
2026-05-25 06:48:37 +0530

    Class 10 html-css

--- javascript/Class-5/main.js
+++ javascript/Class-5/main.js
@@ -1,7 +1,22 @@
-let a = -1;
+// let a = -1;
 
-if(a > 0) {
-    console.log("a is positive");
+// if(a > 0) {
+//     console.log("a is positive");
+// }else{
+//     console.log("a is negative");     
+// }   
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+if(a>b && a>c){
+    console.log("a is greatest");
 }else{
-    console.log("a is negative");     
-}   
+    if(b>c){
+        console.log("b is greatest");
+    }else{
+        console.log("c is greatest");   
+    }
+}
