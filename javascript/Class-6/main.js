207c73a14d2d439693d7c1cb6af8e349dd2bc0de
Tazeemabbas <tazanu2003@gmail.com>
2026-05-25 09:03:27 +0530

    Switch statement

--- javascript/Class-6/main.js
+++ javascript/Class-6/main.js
@@ -1,6 +1,27 @@
-let a = 10, b = 20;
+let week = "Friday";
 
-let minValue ;
-
-minValue = a > b ? b : a;
-console.log(minValue);
+switch (week) {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        break;
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+    case "Saturday":
+        console.log("Today is Saturday");
+        break;
+    case "Sunday":
+        console.log("Today is Sunday");
+        break;  
+    default:
+        console.log("Invalid day");
+}
